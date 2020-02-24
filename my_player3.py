bd4fd5324e6ddf70cd0152a6ea83799e6b045a07
Reebbhaa Mehta <reebbhaa@gmail.com>
2020-02-24 07:55:16 -0800

    first commit

---
+++ my_player3.py
@@ -0,0 +1,83 @@
+# 7042208305 :Reebbhaa Mehta
+from read import readInput
+from write import writeOutput
+
+from host import GO
+import numpy
+
+WIN = 1.0
+LOSS = -1.0
+DRAW = 0.5
+
+"""
+- play game of go through the host
+- against random player & then itself
+- select best moves based on q value tables
+- update Q values after each game
+- update table of q values for each state
+"""
+
+
+class Q_learning_agent:
+    """
+    Q learning agent class should contain:
+    state: current pieces on the board, if it is black or white's turn
+    actions: possible next states. 5x5 board with
+    q_values:
+    constructor:
+        alpha
+        gamma
+        q values: dictionary of state, action tuples as keys and q values as values.
+        initial values
+        states_to_update: list of states visited that need to be updated after a game.
+
+    update_Qvalues:
+
+    next_move:
+
+    select_best_action:
+
+    maxQ:
+    """
+
+    def __init__(self, alpha, gamma, piece_type, initial=0):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.initial_values = initial
+        self.q_values = {}
+        self.states_to_update = []
+        self.piece = piece_type
+
+    def add_state(self, state):
+        if state not in self.q_values:
+            action_mat = numpy.zeros((5, 5))
+            action_mat.fill(self.initial_values)
+            self.q_values[state] = action_mat
+        return
+
+    def select_best_action(self, board, state):
+
+        return
+
+    def next_move(self, board):
+
+        return
+
+    def update_Qvalues(self, result):
+        # after a game update the q table
+        # check result to set the reward
+        self.states_to_update = self.states_to_update[::-1]
+        for i in self.states_to_update:
+            curr_stateQ = self.q_values[i]
+
+
+class GoBoard:
+    """
+    class of go board should contain:
+    constructor:
+        create the board of a fixed size
+    is_move_valid: decide if action is valid
+
+
+    """
+    # def __init__(self):
---
+++ startercode/RandomPlayer.py
@@ -0,0 +1,33 @@
+# Python bytecode 3.6 (3379)
+# Embedded file name: /Users/bo/Documents/projects/561/TA Spring 2020/QL-Example-For-TicTacToe/RandomPlayer.py
+# Compiled at: 2020-01-31 08:30:12
+# Size of source mod 2**32: 661 bytes
+# Decompiled by https://python-decompiler.com
+from Board import Board
+import numpy as np
+
+
+class RandomPlayer:
+
+    def __init__(self, side=None):
+        self.side = side
+
+    def set_side(self, side):
+        self.side = side
+
+    def move(self, board):
+        if board.game_over():
+            return
+        else:
+            candidates = []
+            for i in range(0, 3):
+                for j in range(0, 3):
+                    if board.state[i][j] == 0:
+                        candidates.append(tuple([i, j]))
+
+            idx = np.random.randint(len(candidates))
+            random_move = candidates[idx]
+            return board.move(random_move[0], random_move[1], self.side)
+
+    def learn(self, board):
+        pass
